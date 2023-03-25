a955b20170fe39c70de3c1fc40366659442343dd
Yanglin Zhao <yanglin.zhao@addepar.com>
2023-03-24 20:37:04 -0400

    Full complete

--- lox/scanner.test.ts
+++ lox/scanner.test.ts
@@ -1,12 +1,35 @@
 import { Scanner, TokenType } from "./scanner";
 
 function getTokens(source: string): number[] {
-  return new Scanner(source).scan().map(t => t.type);
+  return new Scanner(source).scan().map((t) => t.type);
 }
 
-test("Scanner basic scenarios", () => {
+test("Scanner should return valid token types", () => {
+  expect(getTokens("")).toEqual([]);
+
+  expect(getTokens("// this is some comment")).toEqual([]);
+
   expect(getTokens("()")).toEqual([
     TokenType.LEFT_PAREN,
     TokenType.RIGHT_PAREN,
   ]);
+
+  expect(getTokens("() == ()")).toEqual([
+    TokenType.LEFT_PAREN,
+    TokenType.RIGHT_PAREN,
+    TokenType.EQUAL_EQUAL,
+    TokenType.LEFT_PAREN,
+    TokenType.RIGHT_PAREN,
+  ]);
+
+  expect(getTokens("(( )){}")).toEqual([
+    TokenType.LEFT_PAREN,
+    TokenType.LEFT_PAREN,
+    TokenType.RIGHT_PAREN,
+    TokenType.RIGHT_PAREN,
+    TokenType.LEFT_BRACE,
+    TokenType.RIGHT_BRACE,
+  ]);
+
+  expect(getTokens('"hello world!"')).toEqual([TokenType.STRING]);
 });
