9d365de637f05c3d0f0c1a4f52ac31bf05f55b99
Yanglin Zhao <yanglin.zhao@addepar.com>
2023-04-07 14:50:30 -0400

    Implement full grammar

--- lox/ast.ts
+++ lox/ast.ts
@@ -1,7 +1,7 @@
 // This file is generated from tools/generator.ts
 // See https://craftinginterpreters.com/representing-code.html#metaprogramming-the-trees
 
-import { Token } from "./scanner";
+import { Token, TokenTypeConstant } from "./scanner";
 
 export class Expr {}
 
@@ -22,15 +22,24 @@ export class ExprGrouping extends Expr {
   // Something goes here!
 }
 
-export class ExprLiteral extends Expr {
-  value: any;
+type ExprLiteralValue = boolean | number | string | null;
 
-  constructor(value: any) {
+export class ExprLiteral extends Expr {
+  value: ExprLiteralValue;
+
+  constructor(value: ExprLiteralValue) {
     super();
     this.value = value;
   }
 }
 
 export class ExprUnary extends Expr {
-  // Something goes here!
+  operator: Token;
+  right: Expr;
+
+  constructor(operator: Token, right: Expr) {
+    super();
+    this.operator = operator;
+    this.right = right;
+  }
 }
--- lox/parser.ts
+++ lox/parser.ts
@@ -70,11 +70,65 @@ export class Parser {
   }
 
   private comparison(): ast.Expr {
-    // TODO: This is the wrong implementation
-    debugger;
+    let expr = this.term();
 
+    while (
+      this.match(
+        TokenType.GREATER,
+        TokenType.GREATER_EQUAL,
+        TokenType.LESS,
+        TokenType.LESS_EQUAL
+      )
+    ) {
+      let operator = this.previous();
+      let right = this.term();
+      expr = new ast.ExprBinary(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  private term(): ast.Expr {
+    let expr = this.factor();
+
+    while (this.match(TokenType.MINUS, TokenType.PLUS)) {
+      let operator = this.previous();
+      let right = this.factor();
+      expr = new ast.ExprBinary(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  private factor(): ast.Expr {
+    let expr = this.unary();
+
+    while (this.match(TokenType.SLASH, TokenType.STAR)) {
+      let operator = this.previous();
+      let right = this.unary();
+      expr = new ast.ExprBinary(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  private unary(): ast.Expr {
+    if (this.match(TokenType.BANG, TokenType.MINUS)) {
+      let operator = this.previous();
+      let right = this.unary();
+      return new ast.ExprUnary(operator, right);
+    }
+
+    return this.primary();
+  }
+
+  private primary(): ast.Expr {
     if (this.match(TokenType.FALSE)) {
       return new ast.ExprLiteral(false);
+    } else if (this.match(TokenType.TRUE)) {
+      return new ast.ExprLiteral(true);
+    } else if (this.match(TokenType.NIL)) {
+      return new ast.ExprLiteral(null);
     }
   }
 }
