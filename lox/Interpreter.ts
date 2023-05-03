f2c340c43aeafb8cb1985ecc55e62d788538c552
Yanglin Zhao <yanglin.zhao@addepar.com>
2023-05-02 20:10:10 -0400

    Add placeholder

---
+++ lox/Interpreter.ts
@@ -0,0 +1,19 @@
+import * as ast from "./Ast";
+
+export class Interpreter extends ast.Visitor {
+  visitExprBinary(expr: ast.ExprBinary): void {
+    // TODO
+  }
+
+  visitExprGrouping(expr: ast.ExprGrouping): void {
+    // TODO
+  }
+
+  visitExprLiteral(expr: ast.ExprLiteral): void {
+    // TODO
+  }
+
+  visitExprUnary(expr: ast.ExprUnary): void {
+    // TODO
+  }
+}
