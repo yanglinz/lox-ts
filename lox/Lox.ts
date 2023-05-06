bf7be1bad140df8ea0918d60c9d1a08d84557e50
Yanglin Zhao <yanglin.zhao@addepar.com>
2023-05-06 13:15:44 -0400

    Move logic into run.ts

--- lox/Lox.ts
+++ lox/Lox.ts
@@ -1,15 +1,9 @@
-import * as fs from "fs/promises";
 import { LoxInstance } from "./Instance";
 import { Scanner } from "./Scanner";
 import { Parser } from "./Parser";
 import { Interpreter } from "./Interpreter";
 
 export class Lox {
-  async runFile(filePath: string): Promise<void> {
-    const contents = await fs.readFile(filePath, "utf8");
-    this.run(contents.toString());
-  }
-
   run(source: string) {
     try {
       const instance = new LoxInstance();
--- tools/run.ts
+++ tools/run.ts
@@ -1,8 +1,15 @@
 import * as path from "path";
-import * as fs from "fs";
+import * as fs from "fs/promises";
 import { Lox } from "../lox/Lox";
 
-function runFile() {
+function fileExists(filePath: string): Promise<boolean> {
+  return fs
+    .access(filePath, fs.constants.F_OK)
+    .then(() => true)
+    .catch(() => false);
+}
+
+async function runFile() {
   // This script naively assumes that it's invoked via `ts-node tools/run.ts [file-path]`
   let filePath = process.argv[2];
   if (!filePath) {
@@ -16,13 +23,15 @@ function runFile() {
   }
 
   // Check if file path exists
-  if (!fs.existsSync(filePath)) {
+  const exists = await fileExists(filePath);
+  if (!exists) {
     console.error(`file ${filePath} does not exist`);
     process.exit(2);
   }
 
+  const source = await fs.readFile(filePath, "utf8");
   const lox = new Lox();
-  lox.runFile(filePath).catch(console.error);
+  lox.run(source);
 }
 
 runFile();
