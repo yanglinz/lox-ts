ab66a6b63b6a6c7a47d0a3a9d9b5ecd989431fe8
Yanglin Zhao <yanglin.zhao@addepar.com>
2023-05-09 20:15:14 -0400

    Implement interface for loggers

--- lox/Instance.ts
+++ lox/Instance.ts
@@ -1,4 +1,31 @@
+class LoggerInterface {
+  messages: string[];
+
+  constructor() {
+    this.messages = [];
+  }
+
+  log(message: string): void {
+    throw new Error("NotImplementedError");
+  }
+}
+
+class ConsoleLogger extends LoggerInterface {
+  log(message: string) {
+    console.log("lox >", message);
+  }
+}
+
+export class RecordedLogger extends LoggerInterface {
+  log(message: string) {
+    this.messages.push(message);
+  }
+}
+
 export class LoxInstance {
-  // TODO: error messages
-  // TODO: print overrides
+  logger: LoggerInterface;
+
+  constructor(logger?: LoggerInterface) {
+    this.logger = logger ? logger : new ConsoleLogger();
+  }
 }
