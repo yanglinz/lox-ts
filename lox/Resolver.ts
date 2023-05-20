7e16253568e17a012400ed00c9d85d5fc76a75e6
Yanglin Zhao <yanglin.zhao@addepar.com>
2023-05-19 20:33:15 -0400

    Begin implementing scopes

--- lox/Resolver.ts
+++ lox/Resolver.ts
@@ -1,11 +1,42 @@
 import { Interpreter } from "./Interpreter";
 import { Visitor } from "./Visitor";
 
+import { Expr } from "./Expr";
+import { Stmt, StmtBlock } from "./Stmt";
+
+type Scope = Map<string, boolean>;
+
 export class Resolver extends Visitor {
   interpreter: Interpreter;
+  scopes: Scope[];
 
   constructor(interpreter: Interpreter) {
     super();
     this.interpreter = interpreter;
+    this.scopes = [];
+  }
+
+  resolve(node: Expr | Stmt): void {
+    node.accept(this);
+  }
+
+  private beginScope(): void {
+    this.scopes.push(new Map());
+  }
+
+  private endScope(): void {
+    this.scopes.pop();
+  }
+
+  resolveAll(nodes: Expr[] | Stmt[]): void {
+    for (let n of nodes) {
+      this.resolve(n);
+    }
+  }
+
+  visitBlockStmt(stmt: StmtBlock): void {
+    this.beginScope();
+    this.resolveAll(stmt.statements);
+    this.endScope();
   }
 }
