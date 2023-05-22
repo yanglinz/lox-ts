70e849029d8675a0d8247415b8ab7aba34271237
Yanglin Zhao <yanglin.zhao@addepar.com>
2023-05-21 20:51:03 -0400

    Implement visit var stmt

--- lox/Resolver.ts
+++ lox/Resolver.ts
@@ -2,7 +2,8 @@ import { Interpreter } from "./Interpreter";
 import { Visitor } from "./Visitor";
 
 import { Expr } from "./Expr";
-import { Stmt, StmtBlock } from "./Stmt";
+import { Token } from "./Scanner";
+import { Stmt, StmtBlock, StmtVar } from "./Stmt";
 
 type Scope = Map<string, boolean>;
 
@@ -28,15 +29,35 @@ export class Resolver extends Visitor {
     this.scopes.pop();
   }
 
-  resolveAll(nodes: Expr[] | Stmt[]): void {
+  private resolveAll(nodes: Expr[] | Stmt[]): void {
     for (let n of nodes) {
       this.resolve(n);
     }
+  }
+
+  private declare(name: Token): void {
+    if (this.scopes.length === 0) return;
+    let scope = this.scopes.at(-1);
+    scope.set(name.lexeme, false);
+  }
+
+  private define(name: Token): void {
+    if (this.scopes.length === 0) return;
+    let scope = this.scopes.at(-1);
+    scope.set(name.lexeme, true);
   }
 
   visitBlockStmt(stmt: StmtBlock): void {
     this.beginScope();
     this.resolveAll(stmt.statements);
     this.endScope();
+  }
+
+  visitVarStmt(stmt: StmtVar): void {
+    this.declare(stmt.name);
+    if (stmt.initializer != null) {
+      this.resolve(stmt.initializer);
+    }
+    this.define(stmt.name);
   }
 }
