5f724d1ea79aa23d87dbc8d4c467bfb5b1890262
Yanglin Zhao <yanglin.zhao@addepar.com>
2023-06-23 16:36:50 -0400

    Disable any

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
      module: true
   },
   rules: {
+    "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-empty-function": "off",
     "@typescript-eslint/no-unused-vars": [
       "warn",
