6e6cde986c429402daa05dddf80d4bef440cf7ec
Yanglin Zhao <yanglin.zhao@addepar.com>
2023-06-22 21:13:07 -0400

    Fix _ variable usage in arguments

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,4 +4,14 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   root: true,
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        args: "none",
+        ignoreRestSiblings: true,
+      },
+    ],
+  },
 };
