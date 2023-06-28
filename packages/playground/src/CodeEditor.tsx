9e8e761fbd65c6cc2f085976c0f67d8985ab3c3e
Yanglin Zhao <hi@yanglinzhao.com>
2023-06-28 13:52:22 -0400

    Add editor styling

--- packages/playground/src/CodeEditor.tsx
+++ packages/playground/src/CodeEditor.tsx
@@ -1,8 +1,12 @@
 export function CodeEditor() {
   return (
-    <div>
+    <div class="py-5 px-5">
       <textarea
-        class="bg-yellow-50 border border-solid border-slate-200 rounded-sm"
+        class="
+          w-full p-2
+          bg-yellow-50 border border-solid border-slate-200 rounded-sm
+          font-mono text-sm
+        "
         name=""
         id=""
         cols={80}
