6fe5926973d8871d319497b0763ddf5512c53f11
Yanglin Zhao <hi@yanglinzhao.com>
2023-06-27 22:24:45 -0400

    Fix formatting

--- packages/playground/src/App.tsx
+++ packages/playground/src/App.tsx
@@ -1,11 +1,9 @@
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold">
-      Hello world!
-    </h1>
+      <h1 className="text-3xl font-bold">Hello world!</h1>
     </div>
-  )
+  );
 }
 
 export const Root = <App />;
