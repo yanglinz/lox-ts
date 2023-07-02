e5bb8013f0d5769e4183848598c1bf5d72568a5a
Yanglin Zhao <hi@yanglinzhao.com>
2023-07-01 20:30:40 -0400

    Fix tests

--- packages/lox-interpreter/lox/Instance.ts
+++ packages/lox-interpreter/lox/Instance.ts
@@ -47,6 +47,14 @@ export class LoxInstance {
     this.hadError = false;
   }
 
+  get streamError(): StreamError[] {
+    return this.stream.filter(s => s.type === "error") as StreamError[];
+  }
+
+  get streamPrint(): StreamPrint[] {
+    return this.stream.filter(s => s.type === "print") as StreamPrint[];
+  }
+
   print(message: string) {
     this.stream.push({ type: "print", message: message });
   }
