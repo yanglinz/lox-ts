1ff27aff1413b659c0fa7d71125eee87eb196103
Yanglin Zhao <hi@yanglinzhao.com>
2023-07-06 21:00:24 -0400

    Implement example selector

--- packages/playground/src/CodeControls.tsx
+++ packages/playground/src/CodeControls.tsx
@@ -1,19 +1,45 @@
 interface CodeControlsProps {
-  onClickRun: (e: MouseEvent) => void;
+  defaultExampleId: string;
+  onChangeExample: (e: Event) => void;
+  onClickRun: (e: Event) => void;
 }
+
+const examples = [
+  ["HelloWorld.lox", "Hello World"],
+  ["Addition.lox", "Simple Addition"],
+];
 
 export function CodeControls(props: CodeControlsProps) {
   return (
-    <div>
-      <button
-        class="
-          bg-stone-100 px-5 py-2
-          border border-solid border-stone-200 rounded-sm
-        "
-        onClick={props.onClickRun}
-      >
-        Run
-      </button>
+    <div class="flex">
+      <div>
+        <button
+          class="
+            bg-stone-100 px-6 py-1
+            border border-solid border-stone-200 rounded-sm
+          "
+          onClick={props.onClickRun}
+        >
+          Run
+        </button>
+      </div>
+
+      <div>
+        <select
+          class="
+            bg-stone-100 px-6 py-2
+            border border-solid border-stone-200 rounded-sm
+          "
+          defaultValue={props.defaultExampleId}
+          onChange={props.onChangeExample}
+        >
+          {examples.map((o) => (
+            <option key={o[0]} value={o[0]}>
+              {o[1]}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
   );
 }
